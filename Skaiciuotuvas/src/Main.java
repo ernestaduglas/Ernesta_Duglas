14dc930ec1e935680185c75645c99269ea9ba29e
Ernesta Duglas <duglas.ernesta@gmail.com>
2017-12-05 16:18:02 +0200

    Scanner added

--- Skaiciuotuvas/src/Main.java
+++ Skaiciuotuvas/src/Main.java
@@ -1,28 +1,33 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Scanner reader = new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		int a = reader.nextInt();
+		System.out.print("Enter second number: ");
+		int b = reader.nextInt();
 
 	}
 	
-	public int sum(int a, int b, int c) {
+	public static int sum(int a, int b, int c) {
 		return a+b+c;
 	}
 	
-	public int substract(int a, int b){
+	public static int substract(int a, int b){
 	return a-b;
 }
-	public double divide(int a, int b){
+	public static double divide(int a, int b){
 	return a/b;
 }
-	public int multiply(int a, int b, int c) {
+	public static int multiply(int a, int b, int c) {
 	return a*b*c;
 }
-	public double sqRoot(int a) {
+	public static double sqRoot(int a) {
 	return Math.sqrt(a);
 }
-	public double power(int a, int b){
+	public static double power(int a, int b){
 	return Math.pow(a,b);
 }
 }
